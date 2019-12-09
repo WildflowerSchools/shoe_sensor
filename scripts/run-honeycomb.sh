7299266e63749095be958af1efc5c937444291c0
Paul DeCoursey <paul@decoursey.net>
2019-12-09 08:53:19 -0600

    use assignment-id vs device_id

--- scripts/run-honeycomb.sh
+++ scripts/run-honeycomb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python -m shoe_sensor.tools.collect_honeycomb -m mac_addresses.txt -c 0 -l INFO -t 5 -a $(cat /boot/wildflower-config.yml | yq -r .device_id) -e capucine
+python -m shoe_sensor.tools.collect_honeycomb -m mac_addresses.txt -c 0 -l INFO -t 5 -a $(cat /boot/wildflower-config.yml | yq -r '.["assignment-id"]') -e capucine
